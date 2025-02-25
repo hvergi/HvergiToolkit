54ddbd6333394e28d2d23f859a2c2ecbbde26a8c
Hvergi <4914651+hvergi@users.noreply.github.com>
2025-02-25 06:02:20 -0500

    Add project files.

---
+++ HvergiToolkit/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace HvergiToolkit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "HvergiToolkit" };
+        }
+    }
+}
